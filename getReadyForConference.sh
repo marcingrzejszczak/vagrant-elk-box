5ee730a6a7d391047d17dbd26e18e2809b77c797
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-01-18 13:53:12 +0100

    Updated the scripts

--- getReadyForConference.sh
+++ getReadyForConference.sh
@@ -2,46 +2,12 @@
 
 set -e
 
-WAIT_TIME="${WAIT_TIME:-5}"
-RETRIES="${RETRIES:-30}"
-SERVICE1_PORT="${SERVICE1_PORT:-8081}"
-SERVICE2_PORT="${SERVICE2_PORT:-8082}"
-SERVICE3_PORT="${SERVICE3_PORT:-8083}"
-SERVICE4_PORT="${SERVICE4_PORT:-8084}"
-ZIPKIN_PORT="${ZIPKIN_PORT:-9411}"
 RUN_VAGRANT="${RUN_VAGRANT:-yes}"
 
 function print_logs() {
     echo -e "\n\nSOMETHING WENT WRONG :( :( \n\n"
     echo -e "\n\nPRINTING LOGS FROM ALL APPS\n\n"
     tail -n +1 -- sleuth-documentation-apps/build/*.log
-}
-
-# ${RETRIES} number of times will try to curl to /health endpoint to passed port $1 and localhost
-function curl_local_health_endpoint() {
-    curl_health_endpoint $1 "127.0.0.1"
-}
-
-# ${RETRIES} number of times will try to curl to /health endpoint to passed port $1 and host $2
-function curl_health_endpoint() {
-    local PASSED_HOST="${2:-$HEALTH_HOST}"
-    local READY_FOR_TESTS=1
-    for i in $( seq 1 "${RETRIES}" ); do
-        sleep "${WAIT_TIME}"
-        curl -m 5 "${PASSED_HOST}:$1/health" && READY_FOR_TESTS=0 && break
-        echo "Fail #$i/${RETRIES}... will try again in [${WAIT_TIME}] seconds"
-    done
-    return $READY_FOR_TESTS
-}
-
-function check_app() {
-    READY_FOR_TESTS="no"
-    curl_local_health_endpoint $1 && READY_FOR_TESTS="yes"
-    if [[ "${READY_FOR_TESTS}" == "no" ]] ; then
-        echo "Failed to start service running at port $1"
-        print_logs
-        exit 1
-    fi
 }
 
 function fail_with_message() {
@@ -65,17 +31,6 @@ fi
 
 # Next run the `./runApps.sh` script to initialize Zipkin and the apps (check the `README` of `sleuth-documentation-apps` for Docker setup info)
 ./runApps.sh
-
-echo -e "\n\nChecking if Zipkin is alive"
-check_app $ZIPKIN_PORT
-echo -e "\n\nChecking if Service1 is alive"
-check_app $SERVICE1_PORT
-echo -e "\n\nChecking if Service2 is alive"
-check_app $SERVICE2_PORT
-echo -e "\n\nChecking if Service3 is alive"
-check_app $SERVICE3_PORT
-echo -e "\n\nChecking if Service4 is alive"
-check_app $SERVICE4_PORT
 
 echo -e "\n\nReady to curl first request"
 
